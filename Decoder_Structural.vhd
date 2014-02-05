e0660b98c6a3ef498d2e383fc7aa0b9613d39b40
sabinpark <C16sabin.park@usafa.edu>
2014-02-05 16:16:18 -0700

    Added the decoder structural vhd file

--- Decoder_Structural.vhd
+++ Decoder_Structural.vhd
@@ -1,18 +1,18 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company:  USAFA - ECE 281
+-- Engineer:  C3C Sabin Park
 -- 
--- Create Date:    16:58:21 02/04/2014 
+-- Create Date:   16:54:17 02/04/2014 
 -- Design Name: 
--- Module Name:    Decoder_Structural - Structural 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Module Name:   Decoder_Structural - Structural 
+-- Project Name:   CE 2
+-- Target Devices:  FPGA
+-- Tool versions:  N/A
+-- Description:   the decoder_structural
 --
--- Dependencies: 
+-- Dependencies:   N/A
 --
--- Revision: 
+-- Revision:  0.01
 -- Revision 0.01 - File Created
 -- Additional Comments: 
 --
@@ -40,9 +40,56 @@ entity Decoder_Structural is
 end Decoder_Structural;
 
 architecture Structural of Decoder_Structural is
-
+	
+	COMPONENT inverter
+	PORT(
+		I : IN std_logic;
+		O : OUT std_logic);
+	END COMPONENT;
+	
+	COMPONENT and3
+	PORT(
+		I0 : IN std_logic;
+		I1 : IN std_logic;
+		I2 : IN std_logic;
+		O : OUT std_logic);
+	END COMPONENT;
+	
+	SIGNAL I0_NOT, I1_NOT : std_logic;
+	
 begin
 
+	I0_inverter: inverter PORT MAP(
+		I => I0,
+		O => I0_NOT);
+		
+	I1_inverter: inverter PORT MAP(
+		I => I1,
+		O => I1_NOT);
+
+	Y0_and3: and3 PORT MAP(
+		I0 => I0_NOT,
+		I1 => I1_NOT,
+		I2 => EN,
+		O => Y0);
+		
+	Y1_and3: and3 PORT MAP(
+		I0 => I0,
+		I1 => I1_NOT,
+		I2 => EN,
+		O => Y1);
+		
+	Y2_and3: and3 PORT MAP(
+		I0 => I0_NOT,
+		I1 => I1,
+		I2 => EN,
+		O => Y2);
+	
+	Y3_and3: and3 PORT MAP(
+		I0 => I0,
+		I1 => I1,
+		I2 => EN,
+		O => Y3);
 
 end Structural;
 
